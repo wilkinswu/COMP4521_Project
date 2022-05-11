aa26cc22ba7b57e396b8d525d5a6633c0c32d1b8
wilkinswu <ywudc@connect.ust.hk>
2022-05-11 23:32:01 +0800

    Connect to google map api

--- app/src/main/java/com/example/comp4521_project/MapsActivity.java
+++ app/src/main/java/com/example/comp4521_project/MapsActivity.java
@@ -1,9 +1,11 @@
 package com.example.comp4521_project;
 
+import android.graphics.Color;
+import android.util.Log;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
-
+import com.google.android.gms.*;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,6 +13,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.comp4521_project.databinding.ActivityMapsBinding;
+//import com.google.maps.android.data.geojson.*;
+//import com.google.maps.android.data.geojson.GeoJsonLayer;
+//import com.google.maps.android.data.geojson.GeoJsonPolygonStyle;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -42,10 +47,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng Kyiv = new LatLng(50.431759, 30.517023);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(Kyiv).title("Marker in Kyiv"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Kyiv, 3F));
+
+//        try {
+//            GeoJsonLayer layer = new GeoJsonLayer(mMap, R.raw.es_geojson, getApplicationContext());
+//
+//            GeoJsonPolygonStyle style = layer.getDefaultPolygonStyle();
+//            style.setFillColor(Color.MAGENTA);
+//            style.setStrokeColor(Color.MAGENTA);
+//            style.setStrokeWidth(1F);
+//
+//            layer.addLayerToMap();
+//
+//        } catch (IOException ex) {
+//            Log.e("IOException", ex.getLocalizedMessage());
+//        } catch (JSONException ex) {
+//            Log.e("JSONException", ex.getLocalizedMessage());
+//        }
     }
 }
