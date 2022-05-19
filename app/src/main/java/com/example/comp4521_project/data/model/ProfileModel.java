2e4137ef808ff4af31b91393a48247aa859cf43e
Lawrence <lawrence.wu.intern@gremod.com>
2022-05-19 11:15:58 +0800

    finish profile except showing blog

--- app/src/main/java/com/example/comp4521_project/data/model/ProfileModel.java
+++ app/src/main/java/com/example/comp4521_project/data/model/ProfileModel.java
@@ -3,14 +3,14 @@ package com.example.comp4521_project.data.model;
 public class ProfileModel {
 
     public class Payload {
-        int uid;
-        String username;
-        String nickname;
-        int account_level;
-        int account_status;
-        String date_modified;
-        String date_creation;
-        String date_last_login;
+        public int uid;
+        public String username;
+        public String nickname;
+        public int account_level;
+        public int account_status;
+        public String date_modified;
+        public String date_creation;
+        public String date_last_login;
 
     }
 
