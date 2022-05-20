06ad615ca99c3a4974796d7c0bfe3a475fb3a25c
wilkinswu <ywudc@connect.ust.hk>
2022-05-20 17:39:34 +0800

    Change dashboard layout, convert location to admin area name.

--- app/src/main/java/com/example/comp4521_project/ui/dashboard/AdapterComment.java
+++ app/src/main/java/com/example/comp4521_project/ui/dashboard/AdapterComment.java
@@ -2,6 +2,9 @@ package com.example.comp4521_project.ui.dashboard;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +15,9 @@ import android.widget.Toast;
 import com.example.comp4521_project.R;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 public class AdapterComment extends BaseAdapter {
     // 然后的话就是在我们的这个位置的话创建我们的get和我们的set方法
@@ -62,9 +67,41 @@ public class AdapterComment extends BaseAdapter {
         // 数据的适配
         holder.commentContent.setText(data.get(i).getContent());
         //holder.commentName.setText();
-        holder.commentName.setText("Name: " + data.get(i).getName() +  "\n" +
-                "Type: " + data.get(i).getType() + "\n" +
-                "From:" + data.get(i).getLocation());
+
+        if (data.get(i).getLocation().startsWith("(") && data.get(i).getLocation().charAt(1) != '0'){
+            Location loc = StrToLocation(data.get(i).getLocation());
+            Geocoder geocoder = new Geocoder(dashboardFragment.getActivity(), Locale.getDefault());
+            List<Address> addresses = null;
+            try {
+                addresses = geocoder.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String area = addresses.get(0).getAdminArea();
+            if (area == null){
+                holder.commentName.setText("Name: " + data.get(i).getName() +  "\n" +
+                        "Type: " + data.get(i).getType() + "\n" +
+                        "From: N/A");
+            }
+            else {
+                holder.commentName.setText("Name: " + data.get(i).getName() +  "\n" +
+                        "Type: " + data.get(i).getType() + "\n" +
+                        "From: " + area);
+            }
+
+        }
+        else if (data.get(i).getLocation().startsWith("(0")) {
+            holder.commentName.setText("Name: " + data.get(i).getName() +  "\n" +
+                    "Type: " + data.get(i).getType() + "\n" +
+                    "From: N/A");
+        }
+        else {
+            holder.commentName.setText("Name: " + data.get(i).getName() +  "\n" +
+                    "Type: " + data.get(i).getType() + "\n" +
+                    "From:" + data.get(i).getLocation());
+        }
+
 
         convertView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,7 +114,7 @@ public class AdapterComment extends BaseAdapter {
                         new LatLng(0, 0),
                         "Comment",
                         "@"+postAuthor+": ");
-                Toast.makeText(context, data.get(i).getContent(),Toast.LENGTH_LONG).show();
+                //Toast.makeText(context, data.get(i).getContent(),Toast.LENGTH_LONG).show();
             }
         });
         return convertView;
@@ -91,6 +128,16 @@ public class AdapterComment extends BaseAdapter {
     public static class ViewHolder{
         TextView commentName;
         TextView commentContent;
+    }
+
+    private Location StrToLocation(String str) {
+        String[] arg = str.split(",");
+        String a = arg[0].trim().replaceAll("\\(", "");
+        String b = arg[1].trim().replaceAll("\\)", "");
+        Location l = new Location("");
+        l.setLatitude(Double.parseDouble(a));
+        l.setLongitude(Double.parseDouble(b));
+        return l;
     }
 }
 
